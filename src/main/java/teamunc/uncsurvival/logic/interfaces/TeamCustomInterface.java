e089c476e6eb54c499c9878a5a4e2ed3857a4c9c
Delsus78 <delsustheugm@gmail.com>
2022-01-17 18:38:22 +0100

    refactor of interfaces goal

--- src/main/java/teamunc/uncsurvival/logic/interfaces/TeamCustomInterface.java
+++ src/main/java/teamunc/uncsurvival/logic/interfaces/TeamCustomInterface.java
@@ -2,16 +2,23 @@ package teamunc.uncsurvival.logic.interfaces;
 
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.Inventory;
+import teamunc.uncsurvival.logic.team.Team;
 
 public class TeamCustomInterface extends GameCustomInterface{
-    public TeamCustomInterface() {
-        super(6);
+
+    private Team team;
+    public TeamCustomInterface(Team team) {
+        super(5);
+        this.team = team;
     }
 
     @Override
     public Inventory update() {
         // TODO interface de team
-        this.inv = Bukkit.createInventory(null,27,name);
+        String teamName = this.team.getChatColor() + this.team.getName();
+
+        String teamNameTrad = this.reduceAt(21) + "       " + teamName;
+        this.inv = Bukkit.createInventory(null,27,name + teamNameTrad);
         return this.inv;
     }
 }
